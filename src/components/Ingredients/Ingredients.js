538037595e3692d9756a6f513b8a07368705c15c
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2023-08-06 18:25:32 +0600

    used usecallback hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,56 @@
-import React from 'react';
-
+import React,{ useState, useCallback} from 'react';
+import IngredientList from "./IngredientList"
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients=()=> {
+
+  const [userIngredients,setUserIngredients]=useState([]);
+  
+  // useEffect(()=>{
+  //   fetch("https://react-hooks-934ba-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json").then(response=>(response.json()).then(
+  //     responseData=>{
+  //       const loadedIngredients=[];
+  //       for(const key in responseData){
+  //         loadedIngredients.push({
+  //           id:key,
+  //           title: responseData[key].title,
+  //           amount:responseData[key].amount
+  //         })
+  //       }
+  //       setUserIngredients(loadedIngredients)
+  //     }
+  //   ))
+
+  // },[])
+
+  const filteredIngredientsHandler=useCallback(filteredIngredient=>{
+    setUserIngredients(filteredIngredient)
+},[])
+
+ 
+  
+  const addIngredientHandler= ingredient =>{
+    fetch('https://react-hooks-934ba-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json',{
+      method:"POST",
+      body: JSON.stringify(ingredient),
+      headers: {"Content-Type":"application/json"}
+    }).then(response=>{
+      response.json().then(responseData=>{
+        setUserIngredients(prevIngredients=>([...prevIngredients,{id:responseData.name,...ingredient}]))
+      })
+    })
+   
+  }
+
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
+        <IngredientList onRemoveItem={()=>{}} ingredients={userIngredients} />
       </section>
     </div>
   );
