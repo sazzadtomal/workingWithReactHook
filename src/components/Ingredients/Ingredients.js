678250b88ed38c4615e1bd95a1ca7957d50fd4ff
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2023-08-09 16:46:03 +0600

    used useReducer hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,13 +1,45 @@
-import React,{ useState, useCallback} from 'react';
+import React,{ useState, useCallback, useReducer} from 'react';
 import IngredientList from "./IngredientList"
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import ErrorModal from "../UI/ErrorModal"
-const Ingredients=()=> {
 
-  const [userIngredients,setUserIngredients]=useState([]);
-  const [isLoading,setIsLoading]=useState(false)
-  const [error, setError]=useState()
+
+const ingredientReducer=(currentIngredients, action)=>{
+           switch (action.type){
+            case "SET":
+              return action.ingredients;
+            case "ADD":
+              return [...currentIngredients,action.ingredient];
+            case "DELETE":
+              return currentIngredients.filter(ing=>ing.id!==action.id)
+            default:
+              throw new Error()
+           }
+}
+
+const httpReducer=(curHttpState,action)=>{
+  switch (action.type){
+    case "SEND":
+      return {loading:true,error: null}
+    case "RESPONSE":
+      return {...curHttpState,loading:false}
+    case "ERROR":
+      return {loading:false,error:action.error}
+    case "CLEAR" :
+      return {...curHttpState,error:null}  
+      default:
+       throw new Error("Something went wrong")
+  }
+}
+
+
+
+
+
+const Ingredients=()=> {
+  const [userIngredient,dispatch]=useReducer(ingredientReducer,[])
+  const [httpState,dispatchHttp]=useReducer(httpReducer,{loading:false, error: null})
   // useEffect(()=>{
   //   fetch("https://react-hooks-934ba-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json").then(response=>(response.json()).then(
   //     responseData=>{
@@ -25,60 +57,57 @@ const Ingredients=()=> {
 
   // },[])
 
-  const filteredIngredientsHandler=useCallback(filteredIngredient=>{
-    setUserIngredients(filteredIngredient)
-},[])
+  const filteredIngredientsHandler=useCallback(filteredIngredients=>dispatch({type: "SET",ingredients:filteredIngredients}),[])
 
  
   
   const addIngredientHandler= ingredient =>{
-    setIsLoading(true)
-    fetch('https://react-hooks-934ba-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.jon',{
+    dispatchHttp({type:"SEND"})
+    fetch('https://react-hooks-934ba-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.jsn',{
       method:"POST",
       body: JSON.stringify(ingredient),
       headers: {"Content-Type":"application/json"}
     }).then(response=>{
-      setIsLoading(false)
+      dispatchHttp({type:"RESPONSE"})
       response.json().then(responseData=>{
-        setUserIngredients(prevIngredients=>([...prevIngredients,{id:responseData.name,...ingredient}]))
+        dispatch( {type:"ADD", ingredient:{ide:responseData.name,...ingredient}})
       })
-    }).catch(error=>{
-          setError(error.message)
-    })
+    }).catch(errorData=>{
+      dispatchHttp({type:"ERROR", error:errorData.message})}
+    )
    
   }
   const removeIngredientHandler= ingredientId =>{
-    setIsLoading(true)
+    dispatchHttp({type:"SEND"})
     fetch(`https://react-hooks-934ba-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients/${ingredientId}.json`,{
       method:"DELETE",
       headers: {"Content-Type":"application/json"}
     }).then(response=>{
-      setIsLoading(false)
-      setUserIngredients(prevIngredients=>
-        prevIngredients.filter(ingredient=>ingredientId!==ingredient.id)
-      )
-    })
+      dispatchHttp({type:"RESPONSE"})
+      dispatch({type:"DELETE", id:ingredientId})
+    }).catch(
+      errorData=>{
+        console.log("error");
+        dispatchHttp({type:"ERROR", error:errorData.message})}
+    )
      
    
   }
 
   const clearError=()=>{
-    setError(null);
-    setIsLoading(false)
+       dispatchHttp({type:"CLEAR"})
   }
 
+  console.log("error state", httpState.error)
 
-
-
-   console.log(userIngredients)
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading} />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
-        <IngredientList onRemoveItem={removeIngredientHandler} ingredients={userIngredients} />
+        <IngredientList onRemoveItem={removeIngredientHandler} ingredients={userIngredient} />
       </section>
     </div>
   );
